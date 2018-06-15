37caa5715610ff2134eb26db6430c42bd58f5fe1
Tyler Watts <tylermwatts@gmail.com>
2018-06-14 20:14:22 -0500

    Add project files.

---
+++ InDeskClone.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace InDeskClone.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
